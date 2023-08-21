01562c27a53a7123e9538b159c9513e206b5cf54
VIVI <vivienlenard00@gmail.com>
2023-08-21 16:01:17 +0200

    no segment fault

--- visuals/minimap.c
+++ visuals/minimap.c
@@ -6,7 +6,7 @@
 /*   By: vlenard <vlenard@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 10:25:30 by vlenard           #+#    #+#             */
-/*   Updated: 2023/08/21 14:41:01 by vlenard          ###   ########.fr       */
+/*   Updated: 2023/08/21 14:44:28 by vlenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	print_wall(t_map *s, int x, int y)
 {
-	if (s->co[y][x] == 6) //actually 0
+	if (!s->co[y][x])
 		return (printf("found no coordinate\n"), -1);
 	if (s->co[y][x] == WALL || s->co[y][x] == CLOSED_DOOR)
 		print_square(s->minimap, x, y, s->mm_square, 0xFFFFFFFF);
