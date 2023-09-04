97bb7865b0e96c7c36b255951b20288ca521ace7
Lena <lennykulichkova@gmail.com>
2023-09-04 14:10:52 +0200

    prepare a few maps

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: ekulichk <ekulichk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 17:07:39 by ekulichk          #+#    #+#             */
-/*   Updated: 2023/09/04 13:17:33 by ekulichk         ###   ########.fr       */
+/*   Updated: 2023/09/04 13:48:43 by ekulichk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,51 +54,51 @@ typedef struct s_map_settings
 	bool			*visited;
 }	t_map_set;
 
-//	parser.c +[]
+//			parser.c
 int			parser(t_map_set *map_set, t_map *map, char **argv, int fd);
 void		fill_in_t_map(t_map_set *map_set, t_map *map);
 int			file_path_check(char *argv);
 int			quick_exit(char *str, int fd);
 
-//	read_map.c +[]
+//			read_map.c
 int			read_map(t_map_set *map_set, int fd);
 int			proceed_line(t_map_set *map_set, char *line);
 int			zero_extend(t_map_set *map_set);
 
-//	create_map.c +
+//			create_map.c
 int			create_map(t_map_set *map_set, char *line);
 int			extention(t_map_set *map_set);
 int			map_extend(t_map_set *map_set);
 int			all_width_extend(t_map_set *map_set);
 t_map_char	convert_char(t_map_set *map_set, char c, int width);
 
-//	identifiers.c +[]
+//			identifiers.c
 int			get_identifier(t_map_set *map_set, char *line);
 int			set_identifier(t_map_set *map_set, char **result);
 char		*iden_copy(char *src);
 
-//	color.c +[]
+//			color.c
 int			get_color(t_map_set *map_set, char side, char *str);
 void		set_color(
 				t_map_set *map_set, char side, char **colors, bool *is_default);
 int			get_channel(char *str, bool *is_default);
 int			get_rgba(int r, int g, int b, int a);
 
-//	map_verify.c +
+//			map_verify.c
 int			map_verify(t_map_set *map_set);
 int			visited_init(t_map_set *map_set);
 bool		dfs(t_map_set *map_set, int y, int x);
 bool		is_visited(t_map_set *map_set, int y, int x);
 void		set_visited(t_map_set *map_set, int y, int x);
 
-//	init_data.c +
+//			init_data.c
 int			map_set_init(t_map_set *map_set);
 void		tex_init(t_map_set *map_set);
 double		init_player_view(char p);
 int			set_textures(t_map_set *map_set, t_map *map);
 int			check_textures(t_map *map);
 
-//	parser_utils.c
+//			parser_utils.c
 void		free_split(char **str, int i);
 void		free_map_set(t_map_set *map_set);
 void		free_sides(t_map_set *map_set);
